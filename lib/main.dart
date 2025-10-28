0afbbccdcf73bcb891fc05261dc568bde6866b21
Thanitsara Umpondol <6431503089@lamduan.mfu.ac.th>
2025-10-28 13:39:36 +0700

    feature_borrow_byTear

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
-
+import 'home_page.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyWidget(), debugShowCheckedModeBanner: false));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Boardgame App',
+      theme: ThemeData(primarySwatch: Colors.orange),
+
+      home: const HomePage(),
+    );
+  }
 }
