bdd74cc2b7d34a1d9fc2e188bbd9ed6c4080c7ee
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-10-29 22:25:06 +0700

    lender : function done

--- lib/Lender/lender_main.dart
+++ lib/Lender/lender_main.dart
@@ -1,5 +1,6 @@
 // lender_main.dart
 
+import 'package:boardgame_app/Lender/see_request.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -80,7 +81,7 @@ class _LenderMainState extends State<LenderMain> with TickerProviderStateMixin {
           physics: const NeverScrollableScrollPhysics(),
           children: const [
             BrowseLender(), // 0
-            LenderSeerequests(), // 1
+            SeeLenderRequests(), // 1
             LenderHistory(), // 2 (หน้าที่เพิ่มใหม่)
             Center(child: Text('')), // 3 (หน้าเปล่าสำหรับ Logout)
           ],
